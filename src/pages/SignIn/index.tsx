f8d006591dc770277b36ced4f9db1fe0afab6f02
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-05-11 14:48:42 -0300

    Importando fontes externas

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 import logo from '../../assets/logo.png';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logo} />
+    <Title>Faça seu logon </Title>
   </Container>
 );
 

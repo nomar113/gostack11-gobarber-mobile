e87b88c01d009799f09acc25af52fbb3ab3b61e7
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-05-13 17:01:40 -0300

    Input & Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
-
 import logo from '../../assets/logo.png';
+import Input from '../../component/Input';
+import Button from '../../component/Button';
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logo} />
     <Title>Faça seu logon </Title>
+    <Input name="email" icon="mail" placeholder="E-mail" />
+    <Input name="password" icon="lock" placeholder="Senha" />
+    <Button
+      onPress={() => {
+        console.log('');
+      }}
+    >
+      Entrar
+    </Button>
   </Container>
 );
 

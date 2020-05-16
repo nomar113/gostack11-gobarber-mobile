caf99632628850515868134765507e3512370001
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-05-15 22:24:52 -0300

    Tela de autenticação

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,25 +1,70 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logo from '../../assets/logo.png';
 import Input from '../../component/Input';
 import Button from '../../component/Button';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountText,
+} from './styles';
 
 const SignIn: React.FC = () => (
-  <Container>
-    <Image source={logo} />
-    <Title>Faça seu logon </Title>
-    <Input name="email" icon="mail" placeholder="E-mail" />
-    <Input name="password" icon="lock" placeholder="Senha" />
-    <Button
+  <>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Image source={logo} />
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+          <Button
+            onPress={() => {
+              console.log('');
+            }}
+          >
+            Entrar
+          </Button>
+          <ForgotPassword
+            onPress={() => {
+              console.log('ForgotPassword');
+            }}
+          >
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+
+    <CreateAccountButton
       onPress={() => {
-        console.log('');
+        console.log('CreateAccountButton');
       }}
     >
-      Entrar
-    </Button>
-  </Container>
+      <Icon name="log-in" size={20} color="#ff9000" />
+      <CreateAccountText>Criar conta</CreateAccountText>
+    </CreateAccountButton>
+  </>
 );
 
 export default SignIn;

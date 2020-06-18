f3d7a2f0ddc05d54aef05a03872db169dce593eb
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-06-18 11:27:23 -0300

    Validação dos formulários

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -6,12 +6,15 @@ import {
   KeyboardAvoidingView,
   Platform,
   TextInput,
+  Alert,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import * as Yup from 'yup';
 
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import getValidationErrors from '../../utils/getValidationErrors';
 import logo from '../../assets/logo.png';
 import Input from '../../component/Input';
 import Button from '../../component/Button';
@@ -24,12 +27,37 @@ import {
   CreateAccountText,
 } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const navigation = useNavigation();
   const formRef = useRef<FormHandles>(null);
-  const passwordInputRef = useRef<TextInpu>(null);
-  const handleSubmit = useCallback((data: object) => {
-    console.log(data);
+  const passwordInputRef = useRef<TextInput>(null);
+  const handleSubmit = useCallback(async (data: SignInFormData) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+      await schema.validate(data, { abortEarly: false });
+      // await signIn({ email: data.email, password: data.password });
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(error);
+        formRef.current?.setErrors(errors);
+        return;
+      }
+      Alert.alert(
+        'Erro na autenticação',
+        'Ocorreu um erro ao fazer login, cheque as credenciais',
+      );
+    }
   }, []);
   return (
     <>
